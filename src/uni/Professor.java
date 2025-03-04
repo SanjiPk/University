a77ac2d207a649a0946a92d5179efdc2436353b3
Sanji <zero.0102003@gmail.com>
2025-03-04 16:45:14 +0330

    finish creating classes

--- src/uni/Professor.java
+++ src/uni/Professor.java
@@ -5,17 +5,17 @@ import java.util.*;
 public class Professor {
     public int id;
     public static ArrayList<Professor> professorList = new ArrayList<>();
-    public int persionID;
+    public int personID;
     public int majorID;
 
-    public Professor(int persionID, int majorID) {
-        this.persionID = persionID;
+    public Professor(int personID, int majorID) {
+        this.personID = personID;
         this.majorID = majorID;
     }
 
     public static Professor findById(int ID) {
         for (Professor professor : professorList)
-            if (ID == professor.persionID)
+            if (ID == professor.personID)
                 return professor;
         return null;
     }
